114a1bca2308d3a19ccd11fcfef7eab938920825
SavioFagundes <144637977+SavioFagundes@users.noreply.github.com>
2024-06-03 19:00:05 -0300

    IncluindoProjeto
    IncluindoProjeto

---
+++ QuartosAlugados/Informacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartosAlugados
+{
+    internal class Informacion
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Informacion(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Name + "," + Email;
+        }
+    }
+}
---
+++ QuartosAlugados/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace QuartosAlugados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented ?");
+            int n = int.Parse(Console.ReadLine());
+            Informacion[] vect = new Informacion[n];
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Rent #"+i + ":");
+                Console.Write("Name:");
+                string name = Console.ReadLine();
+                Console.Write("Email:");
+                string email = Console.ReadLine();
+                Console.Write("Room:");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Informacion(name, email);
+
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ":" + vect[i]);
+                }
+            }
+        }
+    }
+}
